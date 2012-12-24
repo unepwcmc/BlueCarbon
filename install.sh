132f8e83322ed77d66d24741eadb59fc47833bd8
Decio Ferreira <decio.ferreira@decioferreira.com>
2012-12-24 09:08:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,30 @@ export LANG="en_US.utf8"
 # PostgreSQL
 
 command -v psql >/dev/null 2>&1 || {
+  echo "$(date +"%T") Installing PostgreSQL..."
+  echo "---------------------------------"
   sudo apt-get update
   sudo apt-get -y install python-software-properties
   sudo add-apt-repository ppa:pitti/postgresql
   sudo apt-get update
   sudo apt-get -y install vim postgresql-9.2 build-essential g++ make postgresql-server-dev-9.2 libxml2-dev
+
+  # Permissions
+
+  sudo su -c 'echo "local   all             postgres                                trust" >  /etc/postgresql/9.2/main/pg_hba.conf'
+  sudo su -c 'echo "local   all             all                                     trust" >> /etc/postgresql/9.2/main/pg_hba.conf'
+  sudo su -c 'echo "host    all             all             127.0.0.1/32            trust" >> /etc/postgresql/9.2/main/pg_hba.conf'
+  sudo su -c 'echo "host    all             all             ::1/128                 trust" >> /etc/postgresql/9.2/main/pg_hba.conf'
+
+  pg_createcluster 9.2 utf8_cluster
+  sudo /etc/init.d/postgresql restart
 }
 
 # GEOS
 
 command -v geos-config >/dev/null 2>&1 || {
+  echo "$(date +"%T") Installing GEOS..."
+  echo "---------------------------"
   wget -q http://download.osgeo.org/geos/geos-3.3.6.tar.bz2
   tar jxf geos-3.3.6.tar.bz2
   cd geos-3.3.6
@@ -26,7 +40,9 @@ command -v geos-config >/dev/null 2>&1 || {
 # PROJ.4
 
 command -v proj >/dev/null 2>&1 || {
-  cd
+  echo "$(date +"%T") Installing GEOS..."
+  echo "---------------------------"
+  cd $HOME
   wget -q http://download.osgeo.org/proj/proj-4.8.0.tar.gz
   tar xvfz proj-4.8.0.tar.gz
   cd proj-4.8.0
@@ -40,7 +56,9 @@ exit
 # GDAL
 
 command -v gdal-config >/dev/null 2>&1 || {
-  cd
+  echo "$(date +"%T") Installing GDAL..."
+  echo "---------------------------"
+  cd $HOME
   wget -q http://download.osgeo.org/gdal/gdal-1.9.2.tar.gz
   tar xvfz gdal-1.9.2.tar.gz
   cd gdal-1.9.2
@@ -51,28 +69,30 @@ command -v gdal-config >/dev/null 2>&1 || {
 
 # PostGIS
 
-postgis=`psql -Upostgres template_postgis -c "SELECT PostGIS_Full_Version()"`
-if expr "$postgis" : '/.*' > /dev/null; then
-  cd
+postgis=`psql -t -Upostgres template_postgis -c "SELECT PostGIS_Full_Version()"`
+if ! expr "$postgis" : '.*POSTGIS.*' > /dev/null; then
+  echo "$(date +"%T") Installing Postgis..."
+  echo "------------------------------"
+  cd $HOME
   wget -q http://download.osgeo.org/postgis/source/postgis-2.0.2.tar.gz
   tar xvfz postgis-2.0.2.tar.gz
   cd postgis-2.0.2
   ./configure --with-raster --with-topology --with-gui
   make
   sudo make install
-  sudo su - postgres
-  createdb template_postgis
-  createlang plpgsql template_postgis
 
-  # ??? EXIT SSH OR export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+  su postgres -c "createdb template_postgis"
+  su postgres -c "createlang plpgsql template_postgis"
 
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/postgis.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/postgis_comments.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/spatial_ref_sys.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/rtpostgis.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/raster_comments.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/topology.sql
-  psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/topology_comments.sql
+  su postgres -c "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH"
+
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/postgis.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/postgis_comments.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/spatial_ref_sys.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/rtpostgis.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/raster_comments.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/topology.sql"
+  su postgres -c "psql -d template_postgis -f /usr/share/postgresql/9.2/contrib/postgis-2.0/topology_comments.sql"
 fi
 
 # RVM
@@ -91,12 +111,16 @@ command -v rvm >/dev/null 2>&1 || {
 
 # App
 
-#sudo apt-get install libxslt-dev libcurl4-gnutls-dev
+sudo apt-get install libxslt-dev libcurl4-gnutls-dev
 
-#cd /vagrant
-#bundle install
-# COPY cartodb_config.yml
-# CHANGE /etc/postgresql/9.2/main/pg_hba.conf to trust
-#sudo /etc/init.d/postgresql reload
-#rake db:create
-#rails s
+cd /vagrant
+bundle install
+
+if [ -f "/vagrant/config/cartodb_config.yml" ]
+then
+	echo "cartodb_config.yml found."
+else
+	echo "cartodb_config.yml not found. Exiting..."
+fi
+
+echo "...done."
