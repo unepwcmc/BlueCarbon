4eb1939e68d7c0ba1bf6c9c25cb22ce2739130da
migtorres <miguel.torres@unep-wcmc.org>
2013-01-03 16:12:33 +0000

    solved bug validate vs. delete

--- lib/cartodb_query.rb
+++ lib/cartodb_query.rb
@@ -13,24 +13,28 @@ class CartodbQuery
     end
 
     if validation.action == 'validate'
-      sql_part = ';'
+
+      <<-SQL
+UPDATE #{table_name} AS t SET toggle = NULL WHERE ST_Intersects(#{geom},t.the_geom) AND t.toggle = true AND (t.action != 'delete' OR t.action IS NULL);
+
+INSERT INTO #{table_name} (the_geom, action, admin_id#{more_params}, phase, phase_id, prev_phase, toggle)
+  SELECT ST_Multi(ST_Intersection(#{geom}, t.the_geom)) AS the_geom, '#{validation.action}', #{validation.admin_id}#{more_fields}, #{uniq_id}, 1, t.phase AS prev_phase, true
+    FROM #{table_name} t
+    WHERE t.toggle IS NULL
+  UNION ALL
+  SELECT ST_Multi(ST_Difference(t.the_geom, ST_Buffer(ST_Collect(#{geom}),0))) AS the_geom, t.action, t.admin_id#{more_groups}, #{uniq_id}, t.phase_id, t.prev_phase, true
+    FROM #{table_name} t
+    WHERE t.toggle IS NULL
+    GROUP BY t.the_geom, t.action, t.admin_id#{more_groups}, t.phase, t.phase_id, t.prev_phase;
+
+
+
+UPDATE #{table_name} SET toggle = false WHERE toggle IS NULL;
+
+SQL
     else
-      sql_part = <<-SQL
-    UNION ALL
-  SELECT ST_Multi(ST_Difference(dt.polygon, ST_Buffer(ST_Collect(t.the_geom),0))) AS the_geom, '#{validation.action}', #{validation.admin_id}#{more_fields}, #{uniq_id}, 1, t.phase AS prev_phase, true
-    FROM #{table_name} t, (SELECT #{geom} AS polygon) dt
-    WHERE ST_Overlaps(t.the_geom, dt.polygon) AND t.toggle IS NULL
-    GROUP BY t.phase, dt.polygon;
 
-INSERT INTO #{table_name}
-  (the_geom, action, admin_id#{more_params}, phase, phase_id, toggle)
-  SELECT ST_Multi(dt.polygon) AS the_geom, '#{validation.action}', #{validation.admin_id}#{more_fields}, #{uniq_id}, 1, true
-    FROM (SELECT #{geom} AS polygon) dt
-    WHERE (SELECT COUNT(1) FROM #{table_name} t, (SELECT #{geom} AS polygon) dt WHERE ST_Overlaps(t.the_geom, dt.polygon) AND toggle IS NULL) = 0;
-      SQL
-    end
-
-    <<-SQL
+      <<-SQL
 UPDATE #{table_name} AS t SET toggle = NULL WHERE ST_Intersects(#{geom},t.the_geom) AND t.toggle = true;
 
 INSERT INTO #{table_name} (the_geom, action, admin_id#{more_params}, phase, phase_id, prev_phase, toggle)
@@ -42,9 +46,22 @@ INSERT INTO #{table_name} (the_geom, action, admin_id#{more_params}, phase, phas
     FROM #{table_name} t
     WHERE t.toggle IS NULL
     GROUP BY t.the_geom, t.action, t.admin_id#{more_groups}, t.phase, t.phase_id, t.prev_phase
-#{sql_part}
+    UNION ALL
+  SELECT ST_Multi(ST_Difference(dt.polygon, ST_Buffer(ST_Collect(t.the_geom),0))) AS the_geom, '#{validation.action}', #{validation.admin_id}#{more_fields}, #{uniq_id}, 1, t.phase AS prev_phase, true
+    FROM #{table_name} t, (SELECT #{geom} AS polygon) dt
+    WHERE ST_Overlaps(t.the_geom, dt.polygon) AND t.toggle IS NULL
+    GROUP BY t.phase, dt.polygon;
+
+INSERT INTO #{table_name}
+  (the_geom, action, admin_id#{more_params}, phase, phase_id, toggle)
+  SELECT ST_Multi(dt.polygon) AS the_geom, '#{validation.action}', #{validation.admin_id}#{more_fields}, #{uniq_id}, 1, true
+    FROM (SELECT #{geom} AS polygon) dt
+    WHERE (SELECT COUNT(1) FROM #{table_name} t, (SELECT #{geom} AS polygon) dt WHERE ST_Overlaps(t.the_geom, dt.polygon) AND toggle IS NULL) = 0;
+
+
 
 UPDATE #{table_name} SET toggle = false WHERE toggle IS NULL;
-    SQL
+SQL
+    end
   end
 end
--- spec/models/cartodb_validate_spec.rb
+++ spec/models/cartodb_validate_spec.rb
@@ -121,5 +121,110 @@ describe CartodbQuery do
       end
 
     end
+
+    describe 'when there are two geometries that intersect and another that does not' do
+      before(:each) do
+        add_query = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((-2 2, 2 2, 2 -2, -2 -2, -2 2)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query)
+
+        add_query2 = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((3 3, 7 3, 7 7, 3 7, 3 3)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query2)
+
+        add_query3 = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((13 13, 17 13, 17 17, 13 17, 13 13)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query3)
+
+
+        query = CartodbQuery.query('geometries', "ST_GeomFromText('MultiPolygon(((-1 3.5, 6 3.5, 6 1, -1 1, -1 3.5)))',4326)", @validation)
+        ActiveRecord::Base.connection.execute(query)
+      end
+
+      it 'creates 4 more geometries (total of 7)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries;")
+        Integer(check['count']).should ==(7)
+      end
+
+      it 'changes 2 geometries to toggle => false' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = false;")
+        Integer(check['count']).should ==(2)
+      end
+
+      it 'adds the 4 new geometries with toggle => true (total of 5)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true;")
+        Integer(check['count']).should ==(5)
+      end
+    end
+
+    describe 'when there are two geometries that intersect and another that does not' do
+      before(:each) do
+        add_query = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((-2 2, 2 2, 2 -2, -2 -2, -2 2)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query)
+
+        add_query2 = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((3 3, 7 3, 7 7, 3 7, 3 3)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query2)
+
+        add_query3 = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((13 13, 17 13, 17 17, 13 17, 13 13)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query3)
+
+
+        query = CartodbQuery.query('geometries', "ST_GeomFromText('MultiPolygon(((-1 3.5, 6 3.5, 6 1, -1 1, -1 3.5)))',4326)", @validation)
+        ActiveRecord::Base.connection.execute(query)
+      end
+
+      it 'creates 4 more geometries (total of 7)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries;")
+        Integer(check['count']).should ==(7)
+      end
+
+      it 'changes 2 geometries to toggle => false' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = false;")
+        Integer(check['count']).should ==(2)
+      end
+
+      it 'adds the 4 new geometries with toggle => true (total of 5)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true;")
+        Integer(check['count']).should ==(5)
+      end
+    end
+    describe 'when there is a geometry that intersects previous deleted geometry' do
+      before(:each) do
+        add_query = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((-2 2, 2 2, 2 -2, -2 -2, -2 2)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query)
+
+        add_query2 = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((3 3, 7 3, 7 7, 3 7, 3 3)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query2)
+
+        query1 = CartodbQuery.query('geometries', "ST_GeomFromText('MultiPolygon(((0 0, 4 0, 4 4, 0 4, 0 0)))',4326)", @exclusion)
+        ActiveRecord::Base.connection.execute(query1)
+
+        query2 = CartodbQuery.query('geometries', "ST_GeomFromText('MultiPolygon(((-1 3.5, 6 3.5, 6 1, -1 1, -1 3.5)))',4326)", @validation)
+        ActiveRecord::Base.connection.execute(query2)
+        puts add_query, add_query2, query1, query2
+      end
+
+      it 'creates 2 more geometries (total of 4)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries;")
+        Integer(check['count']).should ==(11)
+      end
+
+      it 'changes 2 (of 4) geometries to toggle => false' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = false;")
+        Integer(check['count']).should ==(4)
+      end
+
+      it 'adds the 7 new geometries with toggle => true' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true;")
+        Integer(check['count']).should ==(7)
+      end
+
+      it 'adds the 2 new geometries with action validate' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true AND action = 'validate';")
+        Integer(check['count']).should ==(2)
+      end
+
+      it 'keeps 3 geometries with action delete' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true AND action = 'delete';")
+        Integer(check['count']).should ==(3)
+      end
+    end
   end
 end
