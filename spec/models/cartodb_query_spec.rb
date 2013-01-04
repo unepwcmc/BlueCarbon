e278b13dce339dca78d58b3a3a91c43aafee2676
migtorres <miguel.torres@unep-wcmc.org>
2013-01-04 15:40:08 +0000

    new test: 2 identical geometries

--- spec/models/cartodb_query_spec.rb
+++ spec/models/cartodb_query_spec.rb
@@ -226,9 +226,33 @@ describe CartodbQuery do
         check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = false and notes IS NOT NULL;")
         Integer(check['count']).should ==(1)
       end
-
-
     end
+
+        describe 'when intersects the same geometry' do
+      before(:each) do
+        add_query = "INSERT INTO geometries (the_geom, toggle) VALUES (ST_GeomFromText('MULTIPOLYGON(((0 0,4 0, 4 -4, 0 -4, 0 0)))', 4326), true);"
+        ActiveRecord::Base.connection.execute(add_query)
+
+        query = CartodbQuery.query('geometries', "ST_GeomFromText('MultiPolygon(((0 0, 4 0, 4 -4, 0 -4, 0 0)))',4326)", @addition)
+        ActiveRecord::Base.connection.execute(query)
+      end
+
+      it 'creates 3 more geometries (total of 4)' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries;")
+        Integer(check['count']).should ==(4)
+      end
+
+      it 'changes 1 geometry to toggle => false' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = false;")
+        Integer(check['count']).should ==(1)
+      end
+
+      it 'adds the 3 new geometries with toggle => true' do
+        check = ActiveRecord::Base.connection.select_one("SELECT COUNT(1) AS count FROM geometries WHERE toggle = true;")
+        Integer(check['count']).should ==(3)
+      end
+    end
+
 
 
   end
