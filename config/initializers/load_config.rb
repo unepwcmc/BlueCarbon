7dcf07817a2f82dfafc3a218324bd453aac0a04e
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-12-01 18:29:44 +0000

    use ERB in config.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,5 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+APP_CONFIG = YAML.load(
+  ERB.new(
+    File.read(Rails.root.join('config/config.yml'))
+  ).result
+)[Rails.env]
