6dc38e2502ea1cce8a10e5d452bb549609a7e01c
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-12-02 10:20:53 +0000

    merge carto_db config in config.yml

---
+++ config/initializers/carto_db.rb
@@ -0,0 +1,3 @@
+CartoDb.username = APP_CONFIG['carto_db']['username']
+CartoDb.api_key = APP_CONFIG['carto_db']['api_key']
+
--- config/initializers/cartodb.rb
+++
@@ -1,8 +0,0 @@
-CARTODB_CONFIG = YAML.load(
-  ERB.new(
-    File.read(Rails.root.join('config/cartodb_config.yml'))
-  ).result
-)
-
-CartoDb.username = CARTODB_CONFIG['username']
-CartoDb.api_key = CARTODB_CONFIG['api_key']
